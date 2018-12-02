16aa8ce0884b576884de7e084ef12219824b0a90
Antonio <antoniomejiasv94@gmail.com>
2018-12-02 13:43:50 -0300

    adding policies tests, services tests and controller tests for story, record and auth

--- api/controllers/StoryController.js
+++ api/controllers/StoryController.js
@@ -1,22 +1,15 @@
 import R from 'ramda';
 
-const justValidFields = R.pick(['question', 'record_id']);
+const justValidFields = R.pick(['question']);
 
 class StoryController {
     async create(req, res) {
-        // const { id: claimer } = req.user;
         const { recordId } = req.params;
         const payload = R.merge(justValidFields(req.body), { record_id: recordId });
         const story = await Story.create(payload);
         res.json(201, story);
     }
-    // async create(req, res) {
-    //     // const { id: claimer } = req.user;
-    //     const { recordId } = req.params;
-    //     const payload = R.merge(justValidFields(req.body), { record_id: recordId });
-    //     const story = await Claim.create(payload);
-    //     res.json(201, story);
-    // }
+
     async reply(req, res) {
         const { recordId, storyId } = req.params;
         await Story.reply(recordId, storyId, req.body.answer);
