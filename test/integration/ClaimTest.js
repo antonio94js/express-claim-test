4ccba6e1727f655146539c2adff94fd16b2545ac
Antonio <antoniomejiasv94@gmail.com>
2018-12-02 16:30:30 -0300

    adding integration test for remove and getMy Own Claims

--- test/integration/ClaimTest.js
+++ test/integration/ClaimTest.js
@@ -4,6 +4,8 @@ import supertest from 'supertest';
 import app from '../../server';
 import credentials from './fixtures/userCredentials';
 import claim from './fixtures/claim';
+import jwt from 'jsonwebtoken';
+
 
 let server;
 let request;
@@ -24,11 +26,11 @@ after(() => {
     server.close();
 });
 describe('Claim Workflow test', () => {
+    let claimId = null;
     describe('Create Claim', () => {
         let accessToken
         before(async () => {
             const basePath = '/api/v1/auth'
-            console.log(userCredentials);
             const { body } = await request
                 .post(basePath)
                 .send(userCredentials)
@@ -49,6 +51,7 @@ describe('Claim Workflow test', () => {
             expect(body.attendant_id).to.be.equal(null)
             expect(body).to.have.property('status')
             expect(body.status).to.be.equal('pending')
+            claimId = body.id;
         });
 
         it('Should return 400 if fligth_code is not supplied', async () => {
@@ -62,12 +65,61 @@ describe('Claim Workflow test', () => {
             expect(body).to.have.property('code')
             expect(body.code).to.be.equal('InvalidRequestPayload')
         });
-        // it('Should get 401 when invalid credentials are provided', async () => {
-        //     credentials.password = 'invalidpasssword'
-        //     const { body } = await request
-        //         .post(basePath)
-        //         .send(credentials)
-        //         .expect(401);
-        // });
+    });
+    describe('Get My Claims', () => {
+        let accessToken
+        before(async () => {
+            const basePath = '/api/v1/auth'
+            console.log(userCredentials);
+            const { body } = await request
+                .post(basePath)
+                .send(userCredentials)
+                .expect(200);
+            accessToken = body.accessToken;
+        });
+
+        it('Should return 200 and get My claims', async () => {
+            const userData = jwt.verify(accessToken, process.env.JWT_SECRET);
+            const { body } = await request
+                .get('/api/v1/me/claims')
+                .set('Authorization', `Bearer ${accessToken}`)
+                .send()
+                .expect(200);
+            body.forEach((claim) => {
+                expect(claim).to.have.property('claimer_id')
+                expect(claim.claimer_id).to.be.equal(userData.id)
+            })
+        });
+
+    });
+    describe('Delete Claim', () => {
+        let accessToken
+        before(async () => {
+            const basePath = '/api/v1/auth'
+            console.log(userCredentials);
+            const { body } = await request
+                .post(basePath)
+                .send(userCredentials)
+                .expect(200);
+            accessToken = body.accessToken;
+        });
+
+        it('Should return 204 and remove claim from DB', async () => {
+            await request
+                .delete(`${basePath}/${claimId}`)
+                .set('Authorization', `Bearer ${accessToken}`)
+                .send()
+                .expect(204);
+
+            const { body } = await request
+                .get(`${basePath}/${claimId}`)
+                .set('Authorization', `Bearer ${accessToken}`)
+                .send()
+                .expect(404);
+            
+            expect(body).to.have.property('code')
+            expect(body.code).to.be.equal('ClaimNotFound')
+        });
+
     });
 });
